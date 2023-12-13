1bc200fe45f94714331ce01c72c7a033bc8b7197
Mariana Castilho <mrncstdesign@gmail.com>
2023-12-13 19:58:03 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+export async function Button() {
+  return (
+    <>
+      <button className="rounded-full bg-blue-700 text-white py-2 px-4 font-neueMontrealMedium">
+        Submit
+      </button>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        bgYellow: "#FFFCF5",
+        blueBrand: "#001466",
+        blueBrandLight: "#0C4AC2",
+        transparentText: "rgba(0, 0, 0, 0.70)",
+        transparentBg: "rgba(0, 0, 0, 0.02)",
+        blackText: "#081226",
+        gradientBlue1: "#102857",
+        gradientBlue2: "#063AA0",
+        gradientBlue3: "#0049D5",
+        statusGreen: "#32DE6D",
+      },
+      fontFamily: {
+        formula: ["var(--font-formula)"],
+        neueMontrealRegular: ["var(--font-neueMontrealRegular)"],
+        neueMontrealMedium: ["var(--font-neueMontrealMedium)"],
+        neueMachina: ["var(--font-neueMachina)"],
+      },
+      fontSize: {
+        heading: "48px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
