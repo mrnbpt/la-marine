93b1ac3db72ff50e9631fd235b18cc0fe2d6610a
Mariana Castilho <mrncstdesign@gmail.com>
2023-12-18 16:37:08 -0300

    added vercel analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${inter.className} ${formula.variable} ${neueMachina.variable} bg-bgYellow`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
