8db7c91f56483ca95931f9f837aa0808a18d62c7
Mariana Castilho <mrncstdesign@gmail.com>
2023-12-18 20:16:09 -0300

    Remove comment in Utils

--- utils/dayCalculation.ts
+++ utils/dayCalculation.ts
@@ -1,17 +1,5 @@
 import { format, subDays } from "date-fns";
 
-// const today = new Date();
-// const latestFormattedDate = getUnixTime(today);
-
-// const yyyy = today.getFullYear();
-// const mm = String(today.getMonth() + 1).padStart(2, "0");
-// const dd = String(today.getDate()).padStart(2, "0");
-// const ddMinus7 = String(today.getDate() - 7).padStart(2, "0");
-// const latestDate = `${yyyy}-${mm}-${dd}`;
-// const dayMinusSeven = `${yyyy}-${mm}-${dd}`;
-
-// export { latestDate, ddMinus7 };
-
 const tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate() + 1);
 const sevenDaysAgo = subDays(tomorrow, 7);
