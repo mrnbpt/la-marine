4629972ed0878ebb26fdc04dbd58b50bd6d88bff
Mariana Castilho <mrncstdesign@gmail.com>
2023-12-18 20:15:33 -0300

    Remove button

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-export async function Button() {
-  return (
-    <>
-      <button className="rounded-full bg-blue-700 text-white py-2 px-4 font-neueMontrealMedium">
-        Submit
-      </button>
-    </>
-  );
-}
